1ec7955a384dd0942558d11864a39f176301d70e
Sibghat ullah <sibghatullahk83@gmail.com>
2025-01-01 12:08:18 +0500

    eslint type disable

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,6 +11,12 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
+  {
+    rules: {
+      // Disable the rule for using 'any' explicitly
+      "@typescript-eslint/no-explicit-any": "off",
+    },
+  },
 ];
 
 export default eslintConfig;
